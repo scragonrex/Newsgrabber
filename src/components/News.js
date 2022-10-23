d851eca9801ddd9933d198dde3b3f8626163dabb
scragonrex <scragondragon@gmail.com>
2022-10-23 07:26:02 +0530

    commented

--- src/components/News.js
+++ src/components/News.js
@@ -19,26 +19,56 @@ export class News extends Component {
     document.title=`${this.props.category}-Newsgrabber`;
   
   }
+
+  options = {
+    method: 'GET',
+    headers: {
+      'X-BingApis-SDK': 'true',
+      'X-RapidAPI-Key': 'a47529c91emsha25daef1e66a341p1ba7adjsn5c4aed17e5e6',
+      'X-RapidAPI-Host': 'bing-news-search1.p.rapidapi.com'
+    }
+  }
+
+
   async updatePage() {
     console.log("updatePage")
     this.props.setProgress(10);
     console.log(this.state.page);
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    // let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    //  fetch('https://bing-news-search1.p.rapidapi.com/news?category=sports&cc=IN&safeSearch=Off&textFormat=Raw', options)
+    // .then(response => response.json())
+    // .then(response => console.log(response))
+    // .catch(err => console.error(err));
+    
 
-    let data = await fetch(url);
-    this.props.setProgress(30);
-    let parsedData = await data.json();
-    this.props.setProgress(50);
-    console.log(parsedData);
-    this.setState({
-      articles: parsedData.articles,
-      totalResults: parsedData.totalResults,
+    // let data = await fetch(url);
+    // this.props.setProgress(30);
+    // let parsedData = await data.json();
+    // this.props.setProgress(50);
+    // console.log(parsedData);
+    // this.setState({
+    //   articles: parsedData.articles,
+    //   totalResults: parsedData.totalResults,
      
-    });
-    this.props.setProgress(100);
-  
+    // });
+    // this.props.setProgress(100);
+    let data=await fetch('https://bing-news-search1.p.rapidapi.com/news?category=sports&cc=IN&safeSearch=Off&textFormat=Raw', this.options)
+    // .then(response => response.json())
+    // .then(response => {return (response.json())})
+    // .then(data=>this.setState({
+    //   articles: data.value,
+    //   totalResults: 11,
+    // }))
+    // .catch(err => console.error(err));
+    let parsedData=await data.json();
+    
+    this.setState({
+      articles:parsedData.value,
+    })
+    console.log(parsedData);
+    console.log(this.state.articles);
   }
-  async componentDidMount() {
+   async componentDidMount() {
     this.updatePage();
   }
   handlePrevClick = async () => {
@@ -48,7 +78,8 @@ export class News extends Component {
     console.log(this.state.page);
     this.props.setProgress(10);
     console.log(this.state.page);
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page-1}&pageSize=${this.props.pageSize}`;
+    // let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page-1}&pageSize=${this.props.pageSize}`;
+    let url="https://newsapi.org/v2/top-headlines?sources=google-news-in&apiKey=83f0401628484983a8b6ef6dc468d0e6";
  
     let data = await fetch(url);
     this.props.setProgress(30);
@@ -69,7 +100,7 @@ export class News extends Component {
     console.log(this.state.page);
     this.props.setProgress(10);
     console.log(this.state.page);
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page+1}&pageSize=${this.props.pageSize}`;
+    let url = "https://newsapi.org/v2/top-headlines?sources=google-news-in&apiKey=83f0401628484983a8b6ef6dc468d0e6";
    
     let data = await fetch(url);
     this.props.setProgress(30);
@@ -94,9 +125,10 @@ export class News extends Component {
           <div className="container">
         <div className="row">
         {this.state.articles.map((element) => {
+          let imageEl=element.provider[0].image.thumbnail.contentUrl
             return (
               <div className="col-md-4" key={element.url}>
-                <NewsItem title={element.title ? element.title.slice(0, 45) : ""} description={element.description ? element.description.slice(0, 88) : ""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
+                <NewsItem title={element.name ? element.name.slice(0, 45) : ""} description={element.description ? element.description.slice(0, 88) : ""} imageUrl={imageEl} newsUrl={element.url} author={"author"} date={element.datePublished} source={"source"} />
               </div>);
           })}
         </div></div>
