a5ef01a787e3cb1b0d40c909c418194b5163e8f3
scragonrex <scragondragon@gmail.com>
2022-11-25 18:18:28 +0530

    navbar positon fixed

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -58,7 +58,7 @@ const NavBar = (props)=> {
 
     return (
       <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+      <AppBar position="fixed">
         <Toolbar > 
                 <IconButton
                   size="large"
